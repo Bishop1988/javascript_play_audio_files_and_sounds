56f05d1b7ce64e5db605ee5e2430b51867a49828
Michael Bishop <47477701+Bishop1988@users.noreply.github.com>
2019-08-09 16:19:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const myArray = ["lion","cougar","bark"];
+document.addEventListener("DOMContentLoaded",init);
+function init(){
+    myArray.forEach(function(item){
+        console.log(item);
+        let div = document.createElement("div");
+        div.setAttribute("class","animal "+item);
+        div.innerText = item.toUpperCase();
+        div.addEventListener("click", function () {
+            playit(item);
+        })
+        document.body.appendChild(div);
+    })
+}
+const animals = document.querySelectorAll(".animal");
+for (let i = 0; i < animals.length; i++) {
+    animals[i].addEventListener("click", function () {
+        let animal = this.innerHTML;
+        let lowerAnimal = animal.toLowerCase();
+        playit(lowerAnimal);
+    })
+}
+
+function playit(name) {
+    let activeEle = document.querySelector("." + name);
+    let sound1 = new Audio("sound/"+name+".mp3");
+    sound1.play();
+    activeEle.classList.add("active");
+    setTimeout(function () {
+        activeEle.classList.remove("active");
+    }, 200)
+}
+
+
